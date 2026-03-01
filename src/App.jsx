75c54a07af9ac1e9da283465949f8d647d8492ad
Yosef Hershberg <berioo2000@gmail.com>
2026-03-01 12:23:14 +0200

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -3,45 +3,21 @@ import './App.css'
 import CartPage from './pages/CartPage'
 import Navbar from './components/Navbar'
 import ShopPage from './pages/ShopPage'
-import { createContext, useState } from 'react'
-
-const Context = createContext()
 
 function App() {
-  const [state, setState] = useState()
-  const [cart, setCart] = useState([])
-  const [todos, setTodos] = useState([])
-
-  const addToCart = (item) => {
-    setCart((prevCart) => [...prevCart, item])
-  }
 
   return (
-    <Context.Provider value={{ state, cart, todos }}>
+    <div className="app-shell">
+      <Navbar />
 
-      <div className="app-shell">
-        <Navbar />
-
-        <main>
-          <Routes>
-            <Route path="/" element={<ShopPage />} />
-            <Route path="/cart" element={<CartPage />} />
-          </Routes>
-        </main>
-      </div>
-
-    </Context.Provider>
+      <main>
+        <Routes>
+          <Route path="/" element={<ShopPage />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
 export default App
-
-function ChildComponent() {
-  const { state } = useContext(Context)
-
-  return (
-    <div>
-      <p>State: {state}</p>
-    </div>
-  )
-}
